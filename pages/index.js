c14235ad99c956f8af4e182f818518da153c82b5
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-04-20 16:36:58 +0800

    initial setup for the project and established connection in mongodb

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+function Home({products}) {
+	console.log(products);
+	return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+	const url = "http://localhost:3000/api/products";
+	const response = await axios.get(url);
+	return { products: response.data };
+}
+
+export default Home;
